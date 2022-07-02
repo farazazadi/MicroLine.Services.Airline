efb64ee2859741d38fca97accad4951f681403ee
Faraz Azadi <farazazadi70@gmail.com>
2022-07-03 02:54:54 +0300

    Add encapsulation rules to the Architecture test project

---
+++ Tests/MicroLine.Services.Airline.Tests.Architecture/GeneralRules/EncapsulationRules.cs
@@ -0,0 +1,30 @@
+﻿namespace MicroLine.Services.Airline.Tests.Architecture.GeneralRules;
+
+public class EncapsulationRules
+{
+    [Fact]
+    public void TypesShouldNotHavePublicOrInternalFields()
+    {
+        FieldMembers()
+            .That().ArePublic().Or().AreInternal()
+            .And().AreNotDeclaredIn(Types().That().AreEnums())
+            .Should()
+            .NotExist()
+            .Because("of encapsulation violation")
+            .Check(Assemblies.Architecture);
+    }
+
+
+    [Fact]
+    public void PropertiesShouldNotHavePublicSetter()
+    {
+        PropertyMembers()
+            .That().ArePublic().Or().AreInternal()
+            .Should()
+            .NotHavePublicSetter()
+            .Because("of encapsulation violation")
+            .Check(Assemblies.Architecture);
+    }
+
+
+}

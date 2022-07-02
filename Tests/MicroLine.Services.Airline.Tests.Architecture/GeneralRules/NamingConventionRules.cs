4a55f554b49ef4fad0070726d7542f7edabfc97d
Faraz Azadi <farazazadi70@gmail.com>
2022-07-03 02:52:21 +0300

    Add naming naming convention rules to Architecture test project

---
+++ Tests/MicroLine.Services.Airline.Tests.Architecture/GeneralRules/NamingConventionRules.cs
@@ -0,0 +1,59 @@
+﻿namespace MicroLine.Services.Airline.Tests.Architecture.GeneralRules;
+
+public class NamingConventionRules
+{
+    private static GivenMethodMembersThat Methods() => 
+        MethodMembers()
+            .That()
+            .AreNoConstructors()
+            .And();
+
+
+    [Fact]
+    internal void GetterMethodsShouldNotReturnVoid()
+    {
+        Methods()
+            .HaveName("Get[A-Z].*", true)
+            .Should()
+            .NotHaveReturnType(typeof(void))
+            .Because("of respecting naming convention")
+            .Check(Assemblies.Architecture);
+    }
+
+
+    [Fact]
+    public void IsAndHasMethodsShouldReturnBooleans()
+    {
+        Methods()
+            .HaveName("Is[A-Z].*", true)
+            .Or()
+            .HaveName("Has[A-Z].*", true)
+            .Should()
+            .HaveReturnType(typeof(bool))
+            .Because("of respecting naming convention")
+            .Check(Assemblies.Architecture);
+    }
+
+
+    [Fact]
+    public void SetterMethodsShouldReturnVoid()
+    {
+        Methods()
+            .HaveName("Set[A-Z].*", true)
+            .Should()
+            .HaveReturnType(typeof(void))
+            .Because("of respecting CQS principle and naming convention")
+            .Check(Assemblies.Architecture);
+    }
+
+
+    [Fact]
+    public void InterfaceNameShouldStartWithAnI()
+    {
+        Interfaces()
+            .Should()
+            .HaveNameStartingWith("I")
+            .Because("of respecting naming conventions")
+            .Check(Assemblies.Architecture);
+    }
+}

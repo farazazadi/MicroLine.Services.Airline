8ae7153b910dc7089d4faf652d76d8545c041ef2
Faraz Azadi <farazazadi70@gmail.com>
2022-07-03 03:02:02 +0300

    Add domain rules to the Architecture test project

---
+++ Tests/MicroLine.Services.Airline.Tests.Architecture/Domain/DomainRules.cs
@@ -0,0 +1,91 @@
+﻿namespace MicroLine.Services.Airline.Tests.Architecture.Domain;
+
+public class DomainRules
+{
+    private static GivenTypesConjunction DomainTypes() =>
+        Types()
+        .That()
+        .ResideInAssembly(Assemblies.Domain);
+
+    private static GivenMethodMembersConjunction DomainMethods() =>
+        MethodMembers()
+        .That()
+        .AreDeclaredIn(DomainTypes());
+
+    private static GivenPropertyMembersConjunction DomainProperties() =>
+        PropertyMembers()
+        .That()
+        .AreDeclaredIn(DomainTypes());
+
+    private static GivenFieldMembersConjunction DomainFields() =>
+        FieldMembers()
+        .That()
+        .AreDeclaredIn(DomainTypes());
+
+
+
+    [Fact]
+    public void DomainLayerShouldNotDependOnAnyLayer()
+    {
+        DomainTypes()
+            .Should()
+            .OnlyDependOnTypesThat()
+            .ResideInAssembly(Assemblies.Domain)
+            .Because("of Domain model isolation")
+            .Check(Assemblies.Architecture);
+    }
+
+
+    [Fact]
+    public void DomainTypesShouldNotHaveAnyUnauthorizedAttribute()
+    {
+        DomainTypes()
+            .And()
+            .DoNotResideInNamespace(Namespaces.DomainCommonAndItsInnerNamespacesRegexPattern, true)
+            .Should()
+            .NotHaveAnyAttributesThat()
+            .DoNotResideInNamespace(Namespaces.DomainAllowedAttributesNamespacesRegexPattern, true)
+            .Because("of Domain model isolation")
+            .Check(Assemblies.Architecture);
+    }
+
+
+
+    [Fact]
+    public void DomainPropertiesShouldNotHaveAnyUnauthorizedAttribute()
+    {
+        DomainProperties()
+            .Should()
+            .NotHaveAnyAttributesThat()
+            .DoNotResideInNamespace(Namespaces.DomainAllowedAttributesNamespacesRegexPattern, true)
+            .Because("of Domain model isolation")
+            .Check(Assemblies.Architecture);
+    }
+
+
+
+    [Fact]
+    public void DomainFieldsShouldNotHaveAnyUnauthorizedAttribute()
+    {
+        DomainFields()
+            .Should()
+            .NotHaveAnyAttributesThat()
+            .DoNotResideInNamespace(Namespaces.DomainAllowedAttributesNamespacesRegexPattern, true)
+            .Because("of Domain model isolation")
+            .Check(Assemblies.Architecture);
+    }
+
+
+
+    [Fact]
+    public void DomainMethodsShouldNotHaveAnyUnauthorizedAttribute()
+    {
+        DomainMethods()
+            .Should()
+            .NotHaveAnyAttributesThat()
+            .DoNotResideInNamespace(Namespaces.DomainAllowedAttributesNamespacesRegexPattern, true)
+            .Because("of Domain model isolation")
+            .Check(Assemblies.Architecture);
+    }
+
+}
--- Tests/MicroLine.Services.Airline.Tests.Architecture/Usings.cs
+++ Tests/MicroLine.Services.Airline.Tests.Architecture/Usings.cs
@@ -1,4 +1,7 @@
 global using Xunit;
 global using ArchUnitNET.xUnit;
 global using ArchUnitNET.Fluent.Syntax.Elements.Members.MethodMembers;
+global using ArchUnitNET.Fluent.Syntax.Elements.Members.FieldMembers;
+global using ArchUnitNET.Fluent.Syntax.Elements.Members.PropertyMembers;
+global using ArchUnitNET.Fluent.Syntax.Elements.Types;
 global using static ArchUnitNET.Fluent.ArchRuleDefinition;

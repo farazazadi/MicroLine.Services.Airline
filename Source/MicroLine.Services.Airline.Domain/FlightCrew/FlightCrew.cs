d56058113d3185cc45d378530194cb2401f17a86
Faraz Azadi <farazazadi70@gmail.com>
2022-07-15 21:07:46 +0300

    Add Missing Properties To FlightCrew Aggregate

--- Source/MicroLine.Services.Airline.Domain/FlightCrew/FlightCrew.cs
+++ Source/MicroLine.Services.Airline.Domain/FlightCrew/FlightCrew.cs
@@ -6,12 +6,34 @@ namespace MicroLine.Services.Airline.Domain.FlightCrew;
 
 public class FlightCrew : AggregateRoot
 {
+
+    public FlightCrewType FlightCrewType { get; }
+    public Gender Gender { get; }
+    public FullName FullName { get; }
+    public Date BirtDate { get; }
+    public NationalId NationalId { get; }
+    public PassportNumber PassportNumber { get; }
+    public Email Email { get; }
+    public ContactNumber ContactNumber { get; }
+    public Address Address { get; }
+
+
     private FlightCrew(FlightCrewType flightCrewType, Gender gender, FullName fullName,
                                     Date birtDate, NationalId nationalId, PassportNumber passportNumber,
                                     Email email, ContactNumber contactNumber, Address address)
     {
-
+        FlightCrewType = flightCrewType;
+        Gender = gender;
+        FullName = fullName;
+        BirtDate = birtDate;
+        NationalId = nationalId;
+        PassportNumber = passportNumber;
+        Email = email;
+        ContactNumber = contactNumber;
+        Address = address;
     }
+
+
     public static FlightCrew Create(FlightCrewType flightCrewType, Gender gender, FullName fullName,
                                     Date birtDate, NationalId nationalId, PassportNumber passportNumber,
                                     Email email, ContactNumber contactNumber, Address address)

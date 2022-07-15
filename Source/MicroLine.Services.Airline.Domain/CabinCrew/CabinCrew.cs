22f8e490fed7abcfd955ac2f5f1ea132abc1e947
Faraz Azadi <farazazadi70@gmail.com>
2022-07-16 00:51:06 +0300

    Add CabinCrew Aggregate

---
+++ Source/MicroLine.Services.Airline.Domain/CabinCrew/CabinCrew.cs
@@ -0,0 +1,44 @@
+﻿using MicroLine.Services.Airline.Domain.Common;
+using MicroLine.Services.Airline.Domain.Common.Enums;
+using MicroLine.Services.Airline.Domain.Common.ValueObjects;
+
+namespace MicroLine.Services.Airline.Domain.CabinCrew;
+
+public class CabinCrew : AggregateRoot
+{
+    public CabinCrewType CabinCrewType { get; }
+    public Gender Gender { get; }
+    public FullName FullName { get; }
+    public Date BirtDate { get; }
+    public NationalId NationalId { get; }
+    public PassportNumber PassportNumber { get; }
+    public Email Email { get; }
+    public ContactNumber ContactNumber { get; }
+    public Address Address { get; }
+
+    private CabinCrew(CabinCrewType cabinCrewType, Gender gender, FullName fullName, Date birtDate,
+                      NationalId nationalId, PassportNumber passportNumber,
+                      Email email, ContactNumber contactNumber, Address address)
+    {
+        CabinCrewType = cabinCrewType;
+        Gender = gender;
+        FullName = fullName;
+        BirtDate = birtDate;
+        NationalId = nationalId;
+        PassportNumber = passportNumber;
+        Email = email;
+        ContactNumber = contactNumber;
+        Address = address;
+    }
+
+
+
+    public static CabinCrew Create(CabinCrewType cabinCrewType, Gender gender, FullName fullName, Date birtDate,
+                        NationalId nationalId, PassportNumber passportNumber,
+                        Email email, ContactNumber contactNumber, Address address)
+    {
+        return new CabinCrew(cabinCrewType, gender, fullName, birtDate, nationalId,
+                             passportNumber, email, contactNumber, address);
+    }
+
+}
---
+++ Tests/MicroLine.Services.Airline.Tests.Unit/Domain/CabinCrew/CabinCrewTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using MicroLine.Services.Airline.Domain.CabinCrew;
+using MicroLine.Services.Airline.Domain.Common.Enums;
+using MicroLine.Services.Airline.Domain.Common.ValueObjects;
+
+namespace MicroLine.Services.Airline.Tests.Unit.Domain.CabinCrew;
+public class CabinCrewTests
+{
+    [Fact]
+    public void CabinCrew_ShouldNotHaveAnyEvent_WhenCreated()
+    {
+        // Given
+        // When
+        var cabinCrew = Airline.Domain.CabinCrew.CabinCrew.Create(
+                            cabinCrewType: CabinCrewType.Purser,
+                            gender: Gender.Male,
+                            fullName: FullName.Create("Hanna", "Tremblay"),
+                            birtDate: Date.Create(1985, 5, 17),
+                            nationalId: NationalId.Create("9224150333"),
+                            passportNumber: PassportNumber.Create("A43678998"),
+                            email: Email.Create("test2@gmail.com"),
+                            contactNumber: ContactNumber.Create("+11112223344"),
+                            address: Address.Create("129 Novella Rd", "Toronto", "Ontario", "Canada", "589")
+                            );
+
+        // Then
+        cabinCrew.DomainEvents.Count.Should().Be(0);
+    }
+}

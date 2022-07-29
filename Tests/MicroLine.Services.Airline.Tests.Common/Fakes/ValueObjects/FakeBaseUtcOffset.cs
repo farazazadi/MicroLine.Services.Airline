e73b678386ae670b77570edd5879086ec6319b70
Faraz Azadi <farazazadi70@gmail.com>
2022-07-29 18:50:19 +0300

    Add Airport aggregate fake generators

---
+++ Tests/MicroLine.Services.Airline.Tests.Common/Fakes/ValueObjects/FakeBaseUtcOffset.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+
+namespace MicroLine.Services.Airline.Tests.Common.Fakes.ValueObjects;
+
+public static class FakeBaseUtcOffset
+{
+    public static Domain.Common.ValueObjects.BaseUtcOffset NewFake()
+    {
+        var faker = new Faker();
+
+        var systemTimeZones = TimeZoneInfo.GetSystemTimeZones();
+        var timeZoneInfo = faker.PickRandom(systemTimeZones, 1)
+            .First();
+
+        var hours = timeZoneInfo.BaseUtcOffset.Hours;
+        var minutes = timeZoneInfo.BaseUtcOffset.Minutes;
+
+        return Domain.Common.ValueObjects.BaseUtcOffset.Create(hours, minutes);
+    }
+}
--- Tests/MicroLine.Services.Airline.Tests.Common/RandomSelectionAllowedCharacters.cs
+++ Tests/MicroLine.Services.Airline.Tests.Common/RandomSelectionAllowedCharacters.cs
@@ -4,4 +4,5 @@ internal static class RandomSelectionAllowedCharacters
     internal const string All = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     internal const string DigitsAndLowerCaseLetters = "abcdefghijklmnopqrstuvwxyz0123456789";
     internal const string DigitsAndUpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    internal const string UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }

1c18d94bcce93cc5e53a52d487f0b1e05206cb0e
Faraz Azadi <farazazadi70@gmail.com>
2022-08-14 00:16:40 +0300

    Add Application layer BuildingBlocks

---
+++ Source/MicroLine.Services.Airline.Application/Common/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿namespace MicroLine.Services.Airline.Application.Common.Exceptions;
+internal class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException() : base()
+    {}
+
+    public EntityNotFoundException(string message) : base(message)
+    {}
+
+    public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+    {}
+
+
+    public EntityNotFoundException(string name, object id)
+        : base($"'{name}' ({id}) was not found.")
+    {
+    }
+}
+
---
+++ Source/MicroLine.Services.Airline.Application/Common/Models/DomainEventNotification.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MicroLine.Services.Airline.Domain.Common;
+
+namespace MicroLine.Services.Airline.Application.Common.Models;
+
+public class DomainEventNotification<TDomainEvent> : INotification where TDomainEvent : DomainEvent
+{
+    public TDomainEvent DomainEvent { get; }
+
+    public DomainEventNotification(TDomainEvent domainEvent)
+    {
+        DomainEvent = domainEvent;
+    }
+
+}

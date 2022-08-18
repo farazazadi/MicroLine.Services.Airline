18434040d423d0ff6633e9bc641cb1d4695da2a8
Faraz Azadi <farazazadi70@gmail.com>
2022-08-18 19:39:14 +0300

    Add auditing detail setter methods

--- Source/MicroLine.Services.Airline.Domain/Common/AuditableEntity.cs
+++ Source/MicroLine.Services.Airline.Domain/Common/AuditableEntity.cs
@@ -4,10 +4,22 @@ public abstract class AuditableEntity : Entity
 {
     public string CreatedBy { get; private set; }
 
-    public DateTimeOffset CreatedUtc { get; private set; }
+    public DateTime CreatedAtUtc { get; private set; }
 
     public string LastModifiedBy { get; private set; }
 
-    public DateTimeOffset? LastModifiedUtc { get; private set; }
+    public DateTime? LastModifiedAtUtc { get; private set; }
 
+
+    public void SetModificationDetails(string userId, DateTime utcDateTimeOfModification)
+    {
+        LastModifiedBy = userId;
+        LastModifiedAtUtc = utcDateTimeOfModification;
+    }
+
+    public void SetCreationDetails(string userId, DateTime utcDateTimeOfCreation)
+    {
+        CreatedBy = userId;
+        CreatedAtUtc = utcDateTimeOfCreation;
+    }
 }

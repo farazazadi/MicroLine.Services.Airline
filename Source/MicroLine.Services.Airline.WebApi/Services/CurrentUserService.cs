a541a475c7096f7f597feca6bffdc4e4e2f58ef2
Faraz Azadi <farazazadi70@gmail.com>
2022-08-25 02:54:04 +0300

    Add CurrentUserService.

---
+++ Source/MicroLine.Services.Airline.WebApi/Services/CurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using MicroLine.Services.Airline.Application.Common.Contracts;
+
+namespace MicroLine.Services.Airline.WebApi.Services;
+
+public class CurrentUserService : ICurrentUser
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public string UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+}

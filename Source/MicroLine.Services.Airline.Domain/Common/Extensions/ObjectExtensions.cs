47bf48d79a4875af0a401b487d6b349378e1062b
Faraz Azadi <farazazadi70@gmail.com>
2022-09-08 23:07:27 +0300

    Remove NHibernate proxy support

--- Source/MicroLine.Services.Airline.Domain/Common/Extensions/ObjectExtensions.cs
+++ Source/MicroLine.Services.Airline.Domain/Common/Extensions/ObjectExtensions.cs
@@ -5,12 +5,11 @@ internal static class ObjectExtensions
     internal static Type GetRealType(this object obj)
     {
         const string efCoreProxyPrefix = "Castle.Proxies.";
-        const string nHibernateProxyPostfix = "Proxy";
 
         var type = obj.GetType();
         var typeString = type.ToString();
 
-        if (typeString.Contains(efCoreProxyPrefix) || typeString.EndsWith(nHibernateProxyPostfix))
+        if (typeString.Contains(efCoreProxyPrefix))
             return type.BaseType;
 
         return type;

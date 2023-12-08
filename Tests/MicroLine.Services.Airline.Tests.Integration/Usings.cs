eae777feb9198ac0fb21060a4f26af7e9a945b81
Faraz Azadi <farazazadi70@gmail.com>
2023-12-08 18:50:01 +0330

    Make some usings global to have cleaner test classes

--- Tests/MicroLine.Services.Airline.Tests.Integration/Usings.cs
+++ Tests/MicroLine.Services.Airline.Tests.Integration/Usings.cs
@@ -1,2 +1,6 @@
 global using Xunit;
+global using System.Net.Http.Json;
+global using System.Net;
+global using Microsoft.AspNetCore.Http;
 global using FluentAssertions;
+global using FluentAssertions.ProblemDetail.HttpResponse.Assertions;
